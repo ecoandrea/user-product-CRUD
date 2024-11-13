145acb41afc54e262d0ce5e5fdb6f8ff2219ce2f
aegarmus <aegarmus@gmail.com>
2024-11-13 20:48:06 -0300

    update: agregamos try/catch a createDataFile

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,10 +1,15 @@
 import { createFile, readFile } from "../services/fileService.js"
 
 export const createDataFile = async (data, dataPath) => {
-    const datafile = await readFile(dataPath);
-    let dataJson = null
-
-    !datafile || datafile.length === 0 ? (dataJson = [data]) : dataJson = [ ...datafile, data ]
-
-    await createFile(dataJson, dataPath)
+    try {
+        const datafile = await readFile(dataPath);
+        let dataJson = null
+    
+        !datafile || datafile.length === 0 ? (dataJson = [data]) : dataJson = [ ...datafile, data ]
+    
+        await createFile(dataJson, dataPath)
+        
+    } catch (error) {
+        throw new Error(`Error al gestionar la creación del archivo con la data. ERROR: ${error}`)
+    }
 }
